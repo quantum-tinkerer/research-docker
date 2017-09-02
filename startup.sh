057b69465795e16d5e61ec8c28f7b72ed53e2f41
Joseph Weston <joseph.weston08@gmail.com>
2017-09-02 14:21:38 +0200

    properly setup PATH and other environment for NB_USER
    Without these 'jupyter' will not be in the path and everything will
    explode.

--- startup.sh
+++ startup.sh
@@ -13,5 +13,9 @@ if [[ ! -z "$NB_HOSTNAME" ]]; then
     hostname "$NB_HOSTNAME"
 fi
 
-## start the notebook server
-exec su $NB_USER -c "/usr/local/bin/start-singleuser.sh $*"
+# Ensure that /opt/conda/bin is in the path (we cannot pass PATH to 'su')
+echo 'PATH="/opt/conda/bin:$PATH"' > /etc/profile.d/conda-path
+
+## start the notebook server, passing in all the environment variables
+## we run as NB_USER to prevent 'start.sh' from doing its job twice
+exec su -l $NB_USER -p -c "/usr/local/bin/start-singleuser.sh $*"
