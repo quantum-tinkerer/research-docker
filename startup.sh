d7cd9154d6607b22bc4def8077283b4645cf07a3
Joseph Weston <joseph.weston08@gmail.com>
2017-09-02 21:28:21 +0200

    pass the environment variables in a more idiomatic way

--- startup.sh
+++ startup.sh
@@ -13,9 +13,6 @@ if [[ ! -z "$NB_HOSTNAME" ]]; then
     hostname "$NB_HOSTNAME"
 fi
 
-# Ensure that /opt/conda/bin is in the path (we cannot pass PATH to 'su')
-echo 'PATH="/opt/conda/bin:$PATH"' > /etc/profile.d/conda-path
-
 ## start the notebook server, passing in all the environment variables
 ## we run as NB_USER to prevent 'start.sh' from doing its job twice
-exec su -l $NB_USER -p -c "/usr/local/bin/start-singleuser.sh $*"
+exec su -l $NB_USER -p -c "env PATH=$PATH /usr/local/bin/start-singleuser.sh $*"
