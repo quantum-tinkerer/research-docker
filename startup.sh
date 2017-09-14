99153086674f6ec620682dbe13ed0829c0d8ffec
Joseph Weston <joseph.weston08@gmail.com>
2017-09-14 13:59:28 +0200

    fix environment variables on startup
    set HOME correctly, and don't set hostname from within the container
    (this fails).

--- startup.sh
+++ startup.sh
@@ -8,11 +8,6 @@
 ## launch daemonized supervisor
 /usr/bin/supervisord
 
-## rename the host to something cool
-if [[ ! -z "$NB_HOSTNAME" ]]; then
-    hostname "$NB_HOSTNAME"
-fi
-
 ## start the notebook server, passing in all the environment variables
 ## we run as NB_USER to prevent 'start.sh' from doing its job twice
-exec su $NB_USER -p -c "env PATH=$PATH /usr/local/bin/start-singleuser.sh $*"
+exec su $NB_USER -p -c "env PATH=$PATH HOME=/home/$NB_USER /usr/local/bin/start-singleuser.sh $*"
