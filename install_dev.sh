e103a6dace17e31dd2db32b7f5c7a526496d32fa
Bas Nijholt <basnijholt@gmail.com>
2017-12-18 13:38:00 +0100

    add --yes to the install_dev.sh file

--- install_dev.sh
+++ install_dev.sh
@@ -1,4 +1,4 @@
 conda config --system --add envs_dirs ~/.conda/envs
 source deactivate
-conda env remove --name dev
+conda env remove --yes --name dev
 CONDA_ALWAYS_COPY=true conda env create --file /environments/dev.yml
