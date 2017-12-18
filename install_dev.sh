ed4ddf88c549207482803031791e4d6d3a0ddd2f
Bas Nijholt <basnijholt@gmail.com>
2017-12-18 14:12:31 +0100

    small modification to the install_dev.sh file to not run into space issues

--- install_dev.sh
+++ install_dev.sh
@@ -1,4 +1,6 @@
-conda config --system --add envs_dirs ~/.conda/envs
-source deactivate
+conda clean --yes --all  # so we don't run into space issues
+conda config --system --add envs_dirs ~/.conda/envs  # because of https://gitlab.kwant-project.org/qt/research-docker/issues/4
+source deactivate  # make sure we are not using the dev env now
 conda env remove --yes --name dev
 CONDA_ALWAYS_COPY=true conda env create --file /environments/dev.yml
+conda clean --yes --all  # so we don't run into space issues
