8d06dfee5e11821f85805733d33092e8d58883b7
Anton Akhmerov <anton.akhmerov@gmail.com>
2023-01-13 11:56:54 +0100

    upgrade how we compute the singularity URL

--- main_image/resolve_singularity.py
+++ main_image/resolve_singularity.py
@@ -1,29 +1,44 @@
+from pathlib import Path
+import logging
 import os
 import sys
 import requests
+from ruamel.yaml import YAML
 
-SERVER_URL = "https://gitlab.kwant-project.org"
-PROJECT_ID = 334
-JOB_NAME = "build singularity image"
-IMAGE_PATH = "build/Singularity.simg"
-TARGET_FILENAME = "/etc/singularity_url"
+logging.basicConfig(level=logging.INFO)
+jobs_url = os.getenv("JOBS_URL")
+project_url = jobs_url.split("/pipelines")[0]
+yaml = YAML()
 
+with open(Path(__file__).parent / ".gitlab-ci.yml") as f:
+    ci_config = yaml.load(f)
+for job_name, job in ci_config.items():
+    try:
+        artifact_paths = job["artifacts"]["paths"]
+    except (AttributeError, KeyError, TypeError):
+        continue
+    if image_path := next((p for p in artifact_paths if p.endswith(".simg")), None):
+        break
+else:
+    logging.error(msg="CI does not build a singularity image")
+    sys.exit(250)
+
+
+req = requests.get(os.getenv("JOBS_URL"))
 try:
-    token = os.environ["GITLAB_API_TOKEN"]
-    pipeline_id = os.environ["CI_PIPELINE_ID"]
-except KeyError as ex:
-    print(f"{ex.args[0]} is undefined, not resolving the Singularity container",
-          file=sys.stderr)
-    sys.exit(0)
-
-req = requests.get(f"{SERVER_URL}/api/v4/projects/{PROJECT_ID}/pipelines/{pipeline_id}/jobs",
-                  headers={"PRIVATE-TOKEN": token})
+    req.raise_for_status()
+except requests.exceptions.HTTPError:
+    logging.error(msg="Could not get jobs")
+    sys.exit(250)
 
 for job in reversed(req.json()):
-    if job["name"] == JOB_NAME:
-        with open(TARGET_FILENAME, "w") as f:
-            print(f"{SERVER_URL}/api/v4/projects/{PROJECT_ID}/jobs/{job['id']}/artifacts/{IMAGE_PATH}", file=f)
+    if job["name"] == job_name:
+        singularity_url = f"{project_url}/jobs/{job['id']}/artifacts/{image_path}"
+        logging.info(msg=f"Singularity image URL: {singularity_url}")
+        with open("/etc/singularity_url", "w") as f:
+            print(singularity_url, file=f)
+        os.setenv("SINGULARITY_URL", singularity_url)
         sys.exit(0)
 
-print(f"Job \"{JOB_NAME}\" is not found in the CI job", file=sys.stderr)
+logging.error(msg=f"Job '{job_name}' is not in the pipeline")
 sys.exit(250)
