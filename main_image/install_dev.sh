c8421e5e72425d06bf71abf91d718120481af47e
Anton Akhmerov <anton.akhmerov@gmail.com>
2023-01-16 23:21:48 +0100

    remove unused environments

--- main_image/install_dev.sh
+++
@@ -1,6 +0,0 @@
-conda clean --yes --all  # so we don't run into space issues
-conda config --system --add envs_dirs ~/.conda/envs  # because of https://gitlab.kwant-project.org/qt/research-docker/issues/4
-source deactivate  # make sure we are not using the dev env now
-conda env remove --yes --name dev
-CONDA_ALWAYS_COPY=true conda env create --file /environments/dev.yml
-conda clean --yes --all  # so we don't run into space issues
--- main_image/ipcluster_config_python3.py
+++
@@ -1,4 +0,0 @@
-
-c.LocalControllerLauncher.controller_cmd = ['/opt/conda/bin/python', '-m', 'ipyparallel.controller']
-c.LocalEngineLauncher.engine_cmd = ['/opt/conda/bin/python', '-m', 'ipyparallel.engine']
-c.LocalEngineSetLauncher.engine_cmd = ['/opt/conda/bin/python', '-m', 'ipyparallel.engine']
