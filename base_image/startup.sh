541ab1b7c6f092ef65fbe11073a66c3524d0f0d8
Anton Akhmerov <anton.akhmerov@gmail.com>
2024-12-25 20:00:16 +0100

    make startup.sh a hook

--- base_image/startup.sh
+++ base_image/startup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-/usr/local/bin/start.sh  # rename user etc.
-
-# start.sh does not explicitly set permissions on /home/$NB_USER
-# and if we are mounting this from the docker host for the first
-# time it will have root permissions
-chown $NB_UID:$NB_GID /home/$NB_USER
-cd /home/$NB_USER  # make sure that we're in the right place
-
 # Set the conda environment and package folder in the home folder
 conda config --system --add envs_dirs /home/$NB_USER/.conda/envs
 conda config --system --add pkgs_dirs /home/$NB_USER/.conda/pkgs
@@ -15,7 +7,3 @@ conda config --system --add pkgs_dirs /home/$NB_USER/.conda/pkgs
 ## launch daemonized SSHD
 mkdir -p /var/run/sshd
 /usr/sbin/sshd
-
-## start the notebook server, passing in all the environment variables
-## we run as NB_USER to prevent 'start.sh' from doing its job twice
-exec su $NB_USER -p -c "env PATH=$PATH HOME=/home/$NB_USER jupyterhub-singleuser --ip=0.0.0.0"
